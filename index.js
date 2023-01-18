4c283083c9da183dcd0d6460d2b5f250450d9d74
Ashley Montine Sproul <montinesproul@gmail.com>
2023-01-18 13:49:03 -0500

    created an if statement to select new empoloyee

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Employee = require('./lib/Employee')
 // an array for user input
 const questions = [];
 //to make inquire prompt cleaner
-
+var employeeData = [];
 //Create a function to write HTML file
 function writeToFile(fileName, data) {
     //fs write file goes here (takes js data and creates a literal file on comp)
@@ -45,7 +45,7 @@ function init () {
             name: 'office_number',
         },
         {
-            type: 'checkbox',
+            type: 'list',
             message: 'Would you like to add an Engineer or an Intern?',
             name: 'addToTeam',
             choices: ["Engineer", "Intern", "Finish Building my Team"]
@@ -53,56 +53,62 @@ function init () {
        
     ])
     .then((response) => {
-        console.log(response)
-        const createHTML = generateHTML(response);
-        console.log(createHTML);
-        writeToFile('./dist/index.html', createHTML);
+            var manager = new Manager(response.name, response.employeeID, response.email, response.office_number)
+            employeeData.push(manager);
+            console.log(response);
+            var employee = response.addToTeam;
+        if (employee === "Engineer" ||  employee === "Intern") {
+                 initEngineer(employee);
+        } else {
+            const createHTML = generateHTML(response);
+            console.log(createHTML);
+            writeToFile('./dist/index.html', createHTML);
+        }
     });
     
 }
-function initEngineer () {
-    init();
-    if (employee.addToTeam === Engineer) {
-        inquirer 
-        .prompt([
-            {
-                type: 'input',
-                message: 'Engineers Name',
-                name: 'name',
-            },
-            {
-                type: 'input',
-                message: 'Engineers Employee ID',
-                name: 'employeeID',
-            },
-            {
-                type: 'input',
-                message: 'Engineers Email',
-                name: 'email',
-            },
-            {
-                type: 'input',
-                message: 'Engineers Office Number',
-                name: 'office_number',
-            },
-            {
-                type: 'checkbox',
-                message: 'Would you like to add an Engineer or an Intern?',
-                name: 'addToTeam',
-                choices: ["Engineer", "Intern", "Finish Building my Team"]
-            },
-            
-
-           
-        ])
-    
+function initEngineer (employee) {
+    inquirer 
+    .prompt([
+        {
+            type: 'input',
+            message: `${employee} Name`,
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: `${employee} Employee ID`,
+            name: 'employeeID',
+        },
+        {
+            type: 'input',
+            message: `${employee} Email`,
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: `${employee} Office Number`,
+            name: 'office_number',
+        },
+        {
+            type: 'list',
+            message: 'Would you like to add an Engineer or an Intern?',
+            name: 'addToTeam',
+            choices: ["Engineer", "Intern", "Finish Building my Team"]
+        },
+        
+    ])
     .then((response) => {
         console.log(response)
+        var newEmployee = response.addToTeam;
+       if (newEmployee === "Engineer" || newEmployee === "Intern") {
+            initEngineer();
+       } else {
         const createHTML = generateHTML(response);
         console.log(createHTML);
         writeToFile('./dist/index.html', createHTML);
+       }
     });
 }
-}
 
-initEngineer();
+init();
